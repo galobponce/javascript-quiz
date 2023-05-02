553fc7e842e624128cd04de1392234e205b2258e
galobponce <personal.galo.bello@gmail.com>
2023-05-01 23:33:06 -0300

    Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material"
+
+const App = () => {
+  return (
+    <Typography variant="h3" component="h1">
+      Javascript Quiz
+    </Typography>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { ThemeProvider, CssBaseline } from "@mui/material"
+
+import App from "./App.tsx"
+import theme from "./config/theme.ts"
+
+// Material UI font
+import "@fontsource/roboto/300.css"
+import "@fontsource/roboto/400.css"
+import "@fontsource/roboto/500.css"
+import "@fontsource/roboto/700.css"
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+)
