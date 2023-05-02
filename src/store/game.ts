db8bb0a64ba8bc42c360b1e4ffff8affa9b50e46
galobponce <personal.galo.bello@gmail.com>
2023-05-02 01:28:34 -0300

    Change store name

--- src/store/game.ts
+++ src/store/game.ts
@@ -1,16 +1,20 @@
 import { create } from "zustand"
+
 import { IQuestion } from "../types"
 
-interface QuestionsState {
+interface GameState {
+  hasGameStarted: boolean
   questions: IQuestion[]
   currentQuestion: number
+  startGame: () => void
   fetchQuestions: (limit: number) => Promise<void>
 }
 
-export const useQuestionsStore = create<QuestionsState>((set) => {
+export const useGameStore = create<GameState>((set) => {
   return {
     questions: [],
     currentQuestion: 0,
+    hasGameStarted: false,
 
     fetchQuestions: async (limit) => {
       const res = await fetch("../../intermediate.json")
@@ -19,6 +23,10 @@ export const useQuestionsStore = create<QuestionsState>((set) => {
       // Disorder questions and select limited
       const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
       set({ questions })
+    },
+
+    startGame: () => {
+      set({ hasGameStarted: true })
     },
   }
 })
