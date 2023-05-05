912781147b5be435bf252963e30532348e0d07ce
galobponce <personal.galo.bello@gmail.com>
2023-05-05 12:14:24 -0300

    Fix .json path of fetch

--- src/services/getAllQuestionsByDificulty.ts
+++ src/services/getAllQuestionsByDificulty.ts
@@ -1,6 +1,12 @@
 import { Dificulties } from "../types"
 
 export const getAllQuestionsByDificulty = async (dificulty: Dificulties) => {
-  const res = await fetch(`../../${dificulty}.json`)
-  return await res.json()
+  const res = await fetch(`/${dificulty}.json`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+  const resJson = await res.json()
+  return resJson
 }
