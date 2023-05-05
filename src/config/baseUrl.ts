dd71221f2e3933de15c5c2db32e8b0e7ec3ccf7d
galobponce <personal.galo.bello@gmail.com>
2023-05-05 12:35:00 -0300

    Set config ready for production

---
+++ src/config/baseUrl.ts
@@ -0,0 +1,1 @@
+export const baseUrl = "/javascript-quiz/"
--- src/services/getAllQuestionsByDificulty.ts
+++ src/services/getAllQuestionsByDificulty.ts
@@ -1,7 +1,8 @@
 import { Dificulties } from "../types"
+import { baseUrl } from "../config/baseUrl"
 
 export const getAllQuestionsByDificulty = async (dificulty: Dificulties) => {
-  const res = await fetch(`/${dificulty}.json`, {
+  const res = await fetch(`${baseUrl}${dificulty}.json`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
